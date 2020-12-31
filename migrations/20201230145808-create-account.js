021eb348e2a85b30760d0a6789f4f16d52aa93f0
imam hermawan <immd.flores@gmail.com>
2020-12-31 10:21:30 +0700

    release 2

---
+++ migrations/20201230145808-create-account.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      balance: {
+        type: Sequelize.STRING
+      },
+      customer_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Customers',
+          key: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Accounts');
+  }
+};
---
+++ models/account.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Account.belongsTo(models.Customer, {foreignKey: 'customer_id'})
+    }
+  };
+  Account.init({
+    type: DataTypes.STRING,
+    balance: DataTypes.STRING,
+    customer_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Account',
+  });
+  return Account;
+};
